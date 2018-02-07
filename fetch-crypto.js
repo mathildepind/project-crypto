c14816b3b97b9350958cf2196c3d954af30379c0
mathildepind <mathilde.pind.rasmussen@gmail.com>
2018-02-07 18:04:22 +0000

    showing holding and total price

--- fetch-crypto.js
+++ fetch-crypto.js
@@ -1,10 +1,21 @@
 window.onload = function(){
-  let followedCryptos = ['ETH','BTC','ZEC','DASH','LTC'];
+  let followedCryptos = ['ETH','BTC'];
+  let holdings ={'ETH':2304, 'BTC':10}
   let coinList = JSON.parse(localStorage.getItem('coinList')) || undefined;
   let cryptoNamesArray = Object.keys(coinList);
+  let fullNameToName= {}
+  let cryptoFullNames = Object.keys(coinList).map(key => {
+    let fullName = coinList[key].CoinName
+    fullNameToName[fullName] = key;
+    return fullName;
+  });
+
+  console.log(fullNameToName)
+
+  let cards = document.getElementById('cards');
 
 
-  // Searchinh section
+  // Searching section
   let display = document.getElementById('searchDisplay');
 
   display.addEventListener("click",addToFollowedCryptos);
@@ -44,6 +55,7 @@ window.onload = function(){
     display.setAttribute('class', 'visible');
   }
 
+// Add and remove to followed cryptos
   function addToFollowedCryptos(event) {
     let newCryptoFavourite = event.target.closest('a').textContent;
     if (!followedCryptos.includes(newCryptoFavourite)) {
@@ -56,10 +68,18 @@ window.onload = function(){
     searchBar.value='';
   }
 
+  cards.addEventListener("click", removeFromFollowedCryptos);
+
+  function removeFromFollowedCryptos(event) {
+    let target = event.target;
+    if (target.getAttribute('class') !== 'closeSpan') return;
+    let toRemove = target.getAttribute('id');
+    let removeIndex = followedCryptos.indexOf(toRemove);
+    followedCryptos.splice(removeIndex,1);
+    displayFavourites(followedCryptos);
+  }
+
   //
-
-
-
 
 
 
@@ -74,6 +94,18 @@ window.onload = function(){
     console.log(error);
   })
 
+  function makePricePretty(string){
+    let totalArray = string.split('.')
+    let firstPart= totalArray[0].split('');
+    let newFirstPart=['.'];
+    for(let i =firstPart.length-1;i>=0; i--){
+
+      if(i!==firstPart.length-1 && (firstPart.length-1-i)%3===0) newFirstPart.push(',');
+        newFirstPart.push(firstPart[i]);
+    }
+    return newFirstPart.reverse().concat(totalArray[1]).join('')
+  }
+
   function displayFavourites (followedCryptos){
     let cryptosString= followedCryptos.join();
 
@@ -86,23 +118,32 @@ window.onload = function(){
     })
 
     function createCardsAndAppend(priceList){
-    let cards = document.getElementById('cards');
     cards.innerHTML="";
     followedCryptos.forEach(crypto =>{
       let cryptoObj= coinList[crypto];
 
       let block = document.createElement('div');
       block.setAttribute('class','col-12 col-sm-3');
+
+      let cryptoName = cryptoObj.Name;
+      let priceInPounds = priceList[crypto]['GBP'];
+      let holdingInThisCoin = holdings[crypto];
+      let totalPrice= (holdings[crypto] * priceList[crypto]['GBP']).toFixed(2);
+      totalPrice= makePricePretty(totalPrice)
+
       block.innerHTML = `
           <div class="card">
+            <button type="button" class="close" aria-label="Close">
+              <span class="closeSpan" id=${cryptoName} aria-hidden="true">&times;</span>
+            </button>
           <img class="card-img-top" src=https://www.cryptocompare.com${cryptoObj.ImageUrl} alt=${cryptoObj.CoinName}>
           <div class="card-body text-center">
-            <h5 class="card-title">${cryptoObj.Name}</h5>
-            <p class="card-text current-price">${priceList[crypto]['GBP']}</p>
+            <h5 class="card-title">${cryptoName}</h5>
+            <p class="card-text current-price">£${priceInPounds}</p>
           </div>
           <div class="card-footer">
-            <p class="card-text">Holdings: <span class="holdings"></span></p>
-            <p class="card-text">Total price: <span class="total-holdings"></span></p>
+            <p class="card-text">Holding: <span class="holdings">${holdingInThisCoin} ${cryptoName}</span></p>
+            <p class="card-text">Total value: <span class="total-holdings">£${totalPrice}</span></p>
           </div>
         </div>
       `
